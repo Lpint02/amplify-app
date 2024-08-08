0871b61f2bd96b4ab8d794b32f2697c9a291fda2
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-08-08 17:11:42 +0200

    updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
-
-function App() {
-  const [text, setText] = useState('');
-
-  const handleChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const handleClick = async () => {
-    try {
-        const response = await axios.post('https://rzf142a7hc.execute-api.us-east-1.amazonaws.com/prod/enqueue', {
-            message: text
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        console.log('Response:', response.data);
-    } catch (error) {
-        console.error('Error posting message:', error);
-    }
-};
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <input 
-          type="text" 
-          value={text} 
-          onChange={handleChange} 
-          placeholder="Enter your message"
-        />
-        <button onClick={handleClick}>
-          Send Message
-        </button>
-      </header>
-    </div>
-  );
-}
-
+import React, { useState } from 'react'; 
+import axios from 'axios'; 
+import './App.css'; 
+ 
+function App() { 
+  const [text, setText] = useState(''); 
+  const [responseMessage, setResponseMessage] = useState(''); 
+  const [isError, setIsError] = useState(false); 
+ 
+  const handleChange = (event) => { 
+    setText(event.target.value); 
+  }; 
+ 
+  const handleClick = async () => { 
+    try { 
+        const response = await axios.post('https://rzf142a7hc.execute-api.us-east-1.amazonaws.com/prod/enqueue', { 
+            message: text 
+        }, { 
+            headers: { 
+                'Content-Type': 'application/json' 
+            } 
+        }); 
+        setResponseMessage('Messaggio inviato con successo!'); 
+        setIsError(false); 
+        setText(''); // Svuota la casella di testo
+        console.log('Response:', response.data); 
+    } catch (error) { 
+        setResponseMessage('Errore durante l\'invio del messaggio.'); 
+        setIsError(true); 
+        console.error('Error posting message:', error); 
+    } 
+}; 
+ 
+  return ( 
+    <div className="App"> 
+      <header className="App-header"> 
+        <input  
+          type="text"  
+          value={text}  
+          onChange={handleChange}  
+          placeholder="Enter your message" 
+        /> 
+        <button onClick={handleClick}> 
+          Send Message 
+        </button> 
+        {responseMessage && (
+          <p className={isError ? 'error-message' : 'success-message'}>
+            {responseMessage}
+          </p>
+        )}
+      </header> 
+    </div> 
+  ); 
+} 
+ 
 export default App;
