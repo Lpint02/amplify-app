e428fdbd221578e1705daaa1fdae7cb5e2b3cde7
Leonardo Pinterpe <leonardo.pinterpe@student.univaq.it>
2024-08-08 11:15:21 +0200

    url restored

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   const handleClick = async () => {
     try {
-      const response = await axios.post('YOUR_API_GATEWAY_URL', JSON.stringify({
+      const response = await axios.post('https://rzf142a7hc.execute-api.us-east-1.amazonaws.com/prod/enqueue', JSON.stringify({
         message: text
       }), {
         headers: {
